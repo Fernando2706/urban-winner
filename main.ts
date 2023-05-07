0a01c3766d55b0c24dfd46a31396510cca9b3044
Fernando Murua Alcazar <fernando.murua27@gmail.com>
2023-05-07 17:43:43 +0200

    Change to oak

--- main.ts
+++ main.ts
@@ -1,27 +1,44 @@
 //
-import { Server } from "https://deno.land/std@0.166.0/http/server.ts";
+import { Application, Router } from "https://deno.land/x/oak@v10.0.0/mod.ts";
 import { GraphQLHTTP } from "https://deno.land/x/gql@1.1.2/mod.ts";
 import { makeExecutableSchema } from "https://deno.land/x/graphql_tools@0.0.2/mod.ts";
+import { applyGraphQL, gql, GQLError } from "https://deno.land/x/oak_graphql/mod.ts";
+
 
 import resolvers from "./resolvers/mod.ts";
 import { typeDefs } from "./typedefs.ts";
 
 const schema = makeExecutableSchema({ typeDefs, resolvers });
 
-const server = new Server({
-    handler: async (req) => {
-      const { pathname } = new URL(req.url);
-      // Sleep for 1 second to secure the server
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-      return pathname === "/graphql"
-        ? await GraphQLHTTP<Request>({
-          schema,
-          graphiql: true,
-        })(req)
-        : new Response("Not Found", { status: 404 });
-    },
-    port: 3000,
-  });
+const app = new Application();
 
-    console.log("Server running on http://localhost:3000/graphql");
-    await server.listenAndServe();
+app.use(async (ctx, next) => {
+  await next();
+  const rt = ctx.response.headers.get("X-Response-Time");
+  console.log(`${ctx.request.method} ${ctx.request.url} - ${rt} | from: ${ctx.request.ip}`);
+});
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.response.headers.set("X-Response-Time", `${ms}ms`);
+});
+
+// Add some sleep time
+app.use(async (ctx, next) => {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  await next();
+});
+
+const GraphQLService = await applyGraphQL<Router>({
+  Router,
+  typeDefs: typeDefs,
+  resolvers: resolvers,
+  path: "/graphql",
+})
+
+app.use(GraphQLService.routes(), GraphQLService.allowedMethods());
+
+console.log("Server start at http://localhost:8080");
+await app.listen({ port: 8080 });
